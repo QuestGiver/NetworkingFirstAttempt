0fee38ceab1ad7980cb257cad4494bd6b65931a8
Paul Gibbons <s179054@students.aie.edu.au>
2018-07-18 13:55:05 -0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    // Update is called once per frame
+    void Update()
+    {
+        //------------------------------------------------------------------------------------------
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        //------------------------------------------------------------------------------------------
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, transform.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        Destroy(bullet, 2f);
+
+        NetworkServer.Spawn(bullet);
+
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
